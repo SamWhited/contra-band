74645a15049ee6849d8fdbb4132857239a46ccec
Sam Whited <sam@samwhited.com>
2013-01-28 14:30:24 -0500

    Add the moves list and a few example moves

--- js/main.js
+++ js/main.js
@@ -7,4 +7,15 @@ $(function() {
     }
   });
 
+  // When a move is selected
+  var movesSelect = $('#moves select'),
+      moveAddButton = $('#moves #addButton');
+  movesSelect.change(function() {
+    if (movesSelect.val() !== null) {
+      moveAddButton.removeAttr('disabled');
+    } else {
+      moveAddButton.attr('disabled', 'disabled');
+    }
+  });
+
 });
